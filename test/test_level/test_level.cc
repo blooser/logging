b3401e1aad982a08e4963fd73bb2dc9d9a8aed37
blooser <Blooser@protonmail.com>
2020-11-27 22:20:31 +0100

    Test LevelCast
    Signed-off-by: blooser <Blooser@protonmail.com>

---
+++ test/test_level/test_level.cc
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE Test Level 
+
+#include "../../core/level.h"
+
+#include <boost/test/included/unit_test.hpp> 
+
+using namespace logging;
+
+BOOST_AUTO_TEST_CASE(test_levelcast_cast_to_string) {
+	BOOST_CHECK(LevelCast(Level::INFO).toString() == "INFO");		
+}
